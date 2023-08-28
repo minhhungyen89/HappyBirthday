d3fcc5b28cebed75aee0b06717cdeb3d912640c0
minhhungyen89 <127161903+minhhungyen89@users.noreply.github.com>
2023-08-29 00:56:23 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Hải Anh';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100063915546217';
+const eventName = 'Happy Brithday ';
+const titleCard = 'Nguyễn Thị Hải Anh';
+const contentCard = "Happy birthday to me! Chúc tôi tuổi 19 thật nhiều niềm vui và sức khỏe! Cảm ơn vì những cố gắng không ngừng nghỉ của năm cũ! Mong rằng năm 19 tuổi sẽ thành công hơn trên con đường đã chọn, sẽ tìm được mảnh còn thiếu của bản thân…!!";
+const giftImage = 'FaceHaiAnh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
